ffcc83508091ae6e9b543adcd916c2c2fe0162df
Linda Njau <lindanjau21@gmail.com>
2024-02-26 14:34:18 +0300

    mapping through data with hooks & adding animal breed

--- Pet.jsx
+++ Pet.jsx
@@ -1,5 +1,5 @@
-const Pet = (props) => {
-    return(
+  const Pet = (props) => {
+    return (
         <div>
             <h1>{props.name}</h1>
             <h1>{props.animal}</h1>
@@ -8,3 +8,4 @@ const Pet = (props) => {
     );
   };
 export default Pet;
+ 
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { createRoot } from 'react-dom/client';
-import Pet from './Pet';
-
+import ReactDOM from 'react-dom';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="dog" breed="Havanese" />
-    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-    <Pet name="Dounk" animal="cat" breed="mixed" />
-  </div>
+    return(
+        <div>
+               <h1>Adopt me!</h1>
+        <SearchParams />;
+        </div>
+    );
 };
 const container = document.getElementById("root");
-const root = createRoot(container);
+const root = ReactDOM.createRoot(container);
 root.render(<App />);
