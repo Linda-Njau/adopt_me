4553bcbd9ddfcaaa23e999482ddff4d5ee8f8dba
Linda Njau <lindanjau21@gmail.com>
2024-03-01 17:46:46 +0300

    Carousel.jsx

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import Carousel from './Carousel';
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 
@@ -13,10 +14,11 @@ const Details = () => {
                 <h2 className="loader">🌀</h2>
             </div>
         );
-    }
+    }  
     const pet = results.data.pets[0];
     return (
         <div className="details">
+            <Carousel images={pet.images} />
             <div>
                 <h1>{pet.name}</h1>
                 <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
